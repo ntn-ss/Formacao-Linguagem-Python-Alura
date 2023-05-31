8af479c418e38b0ae40a843e9a65eadc42af486e
ntn-ss <91851915+ntn-ss@users.noreply.github.com>
2023-05-31 10:44:27 -0300

    Upload Primeiro Arquivo

---
+++ testes_iniciais.py
@@ -0,0 +1,10 @@
+print("\n")
+print("Olá, mundo")
+
+pais = "Brasil"
+print(type(pais))
+
+qtdCopas:int = 5
+print(type(qtdCopas))
+
+print(pais, "ganhou", qtdCopas , "Copas do Mundo.", end="\n\n\n")
