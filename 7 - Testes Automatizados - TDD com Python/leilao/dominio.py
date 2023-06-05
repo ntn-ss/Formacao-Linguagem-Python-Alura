af285d31de17f3db0ef8eb42bd9a473cdabe22e1
ntn-ss <91851915+ntn-ss@users.noreply.github.com>
2023-06-05 19:44:16 -0300

    Upload Parte 3 Módulo 7

--- 7 - Testes Automatizados - TDD com Python/leilao/dominio.py
+++ 7 - Testes Automatizados - TDD com Python/leilao/dominio.py
@@ -17,25 +17,21 @@ class Leilao:
     def __init__(self, descricao):
         self.descricao = descricao
         self.__lances = []
+        
+        # estamos pegando, do módulo sys, que se integra ao sistema operacional, o menor valor que um float pode ter. Estamos fazendo o mesmo com o maior valor possível e declarando esses valores a maior_lance e menor_lance. Inicializando as variáveis assim, temos (quase) certeza de que qualquer valor maior ou menor oferecido no leilão será incluído e não quebrará as condições.
+        self.maior_lance = sys.float_info.min
+        self.menor_lance = sys.float_info.max
     
     def propoe(self, lance: Lance):
-        self.__lances.append(lance)
+        # verificar uma lista significa retornar falso se ele for vazio. Com um not, ela estiver vazia, retorna verdadeiro.
+        if not self.__lances or self.__lances[-1].usuario != lance.usuario:
+            if lance.valor > self.maior_lance:
+                self.maior_lance = lance.valor
+            if lance.valor < self.menor_lance:
+                self.menor_lance = lance.valor
+            self.__lances.append(lance)
     
     @property
     def lances(self):
         # chama-se "cópia rasa de lista", queremos impedir que classes externas modifiquem o vetor de lances diretamente.
         return self.__lances[:]
-
-class Avaliador:
-    def __init__(self):
-        # estamos pegando, do módulo sys, que se integra ao sistema operacional, o menor valor que um float pode ter. Estamos fazendo o mesmo com o maior valor possível e declarando esses valores a maior_lance e menor_lance. Inicializando as variáveis assim, temos (quase) certeza de que qualquer valor maior ou menor oferecido no leilão será incluído e não quebrará as condições.
-        self.maior_lance = sys.float_info.min
-        self.menor_lance = sys.float_info.max
-
-    # tipando parâmetro de função
-    def avalia(self, leilao: Leilao):
-        for lance in leilao.lances:
-            if lance.valor > self.maior_lance:
-                self.maior_lance = lance.valor
-            if lance.valor < self.menor_lance:
-                self.menor_lance = lance.valor
--- 7 - Testes Automatizados - TDD com Python/leilao/test_leilao.py
+++ 7 - Testes Automatizados - TDD com Python/leilao/test_leilao.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
-from dominio import Usuario, Leilao, Lance, Avaliador
+from dominio import Usuario, Leilao, Lance
 
-class TestAvaliador(TestCase):
+class TestLeilao(TestCase):
     # o método setUp é executado por padrão antes de qualquer outro teste. Preferir pôr apenas o que for comum a tudo quantidade possível, pois se forem códigos demais (ex.: instâncias de objetos), a máquina pode ficar lenta.
     def setUp(self):
         self.nathan = Usuario('Nathan')
@@ -19,14 +19,11 @@ class TestAvaliador(TestCase):
         
         self.leilao.propoe(lance_do_breno)
         
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
-        
         menor_valor_esperado = 300.00
         maior_valor_esperado = 300.01
         
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
         
         
     # segundo caso de uso, ordem decrescente.
@@ -37,22 +34,16 @@ class TestAvaliador(TestCase):
         self.leilao.propoe(lance_do_breno)
         self.leilao.propoe(self.lance_do_nathan)
         
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
-        
         menor_valor_esperado = 300.00
         maior_valor_esperado = 300.01
         
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
         
     def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_o_leilao_tiver_um_lance (self):
                 
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
-        
-        self.assertEqual(300.0,avaliador.menor_lance)   
-        self.assertEqual(300.0,avaliador.maior_lance)
+        self.assertEqual(300.0,self.leilao.menor_lance)   
+        self.assertEqual(300.0,self.leilao.maior_lance)
     
     # a partir daqui, torna-se inútil testar essa funcionalidade pois é tudo muito parecido e podemos ter confiança de que a quantidade de itens independe, salvo se houver uma regra de negócio como "lance de número X recebe bonificação".
     def test_deve_retornar_o_maior_e_o_menor_valor_quando_o_leilao_tiver_tres_lances (self):
@@ -65,11 +56,35 @@ class TestAvaliador(TestCase):
         self.leilao.propoe(lance_do_breno)
         self.leilao.propoe(lance_do_thales)
         
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
-        
         menor_valor_esperado = 299.99
         maior_valor_esperado = 300.01
         
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+        
+    '''
+    novas regras de negócio:
+    
+    se o leilão não tiver lances, deve permitir propor um lance.
+    '''
+    def test_deve_permitir_propor_um_lance_caso_o_leilao_nao_tenha_lances(self):
+        # o setUp já propoe o lance_do_nathan.
+        quantidade_de_lances_recebidos = len(self.leilao.lances)
+        self.assertEqual(1, quantidade_de_lances_recebidos)
+        
+    # se o último usuário for diferente, deve permitir propor um lance.
+    def test_deve_permitir_propor_um_lance_caso_o_ultimo_usuario_seja_diferente(self):
+        breno = Usuario("Breno")
+        lance_do_breno = Lance(breno, 400.0)
+        
+        self.leilao.propoe(lance_do_breno)
+        quantidade_de_lances_recebidos = len(self.leilao.lances)
+        self.assertEqual(2, quantidade_de_lances_recebidos)
+    # se o último usuário for o mesmo, deve negar propor um lance.
+    def test_deve_negar_propor_lance_caso_o_usuario_seja_o_mesmo(self):
+        lance_do_nathan_de_quatrocentos = Lance(self.nathan, 400.0)
+        
+        self.leilao.propoe(lance_do_nathan_de_quatrocentos)
+        
+        quantidade_de_lances_recebidos = len(self.leilao.lances)
+        self.assertEqual(1, quantidade_de_lances_recebidos)
